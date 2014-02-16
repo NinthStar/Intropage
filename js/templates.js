d3dcafad12ca977f842e7bd78abcecc6e71a07a5
NinthStar <NinthStar942653@gmail.com>
2014-02-16 19:40:42 +0800

    Fix display bug in IE8

--- js/templates.js
+++ js/templates.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 	$("#intro").fadeTo(0, 0);
 	
 	backgroundImageResize();
-	
 	$(window).bind("resize", backgroundImageResize);
 	$("#logo").bind({"mouseenter" : logoOnEnter,
 					 "mouseleave" : logoOnLeave});
@@ -22,20 +21,22 @@ function backgroundImageResize() {
 							  "height" : parseInt(prop *  567) + "px"});
 	$("#logo").css({"width"  : parseInt(prop * 250) + "px",
 					"height" : parseInt(prop * 250) + "px",
-					"top"    : parseInt(prop *  60) + "px"});
-	$("#intro").css({"width"      : parseInt(prop *  720) + "px",
-					 "margin-top" : parseInt(prop * -180) + "px"});
+					"top"    : parseInt(prop *  60) + "px",
+					"left"   : (width - parseInt(prop * 250)) / 2 + "px"});
+	$("#intro").css({"width" : parseInt(prop *  720) + "px",
+					 "top"   : parseInt(prop * -180) + "px",
+					 "left"  : (width - parseInt(prop * 720)) / 2 + "px"});
 	$(".intro-elem p").css("margin-top", parseInt(prop * 30) + "px");
-	$("#command-bar").css("margin-top", parseInt(prop * 120) + "px");
+	$("#command-bar").css("margin-top", parseInt(prop * 0) + "px");
 }
+
 function logoOnEnter() {
-	$("#background-img").stop().animate({opacity : 0.7}, 200);
-	$("#logo").stop().animate({opacity : 1}, 200);
+	$("#background-img").stop().fadeTo(200, 0.7);
+	$("#logo").stop().fadeTo(200, 1);
 	$("#intro").stop().fadeTo(200, 1);
-	$(".intro-elem").stop().animate("position", "absolute");
 }
 function logoOnLeave() {
-	$("#background-img").stop().animate({opacity : 1.2}, 200);
-	$("#logo").stop().animate({opacity : 0.5}, 200);
+	$("#background-img").stop().fadeTo(200, 1);
+	$("#logo").stop().fadeTo(200, 0.5);
 	$("#intro").stop().fadeTo(200, 0);
 }
